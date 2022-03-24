27b9d37f5dd26b8a377382bc3e9494c05a0fdf6d
melinareygil <100168785+melinareygil@users.noreply.github.com>
2022-03-24 17:15:54 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -2,10 +2,10 @@
 <body>
 
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["lname"]; ?>
+Your email address is: <?php echo $_POST["lname"]; ?><br>
  
- <?php echo $_POST["email"]; ?>
- <?php echo $_POST["coments"]; ?>
+ <?php echo $_POST["email"]; ?><br>
+ <?php echo $_POST["coments"]; ?><br>
  
  
  
